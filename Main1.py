90a9aca92ec8aa31ee178a8368d83c3f128dfc10
Elizabeth-124 <elizabeth124.1234@gmail.com>
2025-11-18 19:55:34 +0000

    first commit

---
+++ Main1.py
@@ -0,0 +1,4 @@
+age=19
+if age>=18:
+    print("You are legally an adult")
+print("The code is completed")
---
+++ main2.py
@@ -0,0 +1,9 @@
+score=int(input("Enter your score"))
+if score<=39:
+    print("you have failed")
+elif score <= 49:
+    print("Your grade is D")
+elif score <= 59:
+    print("Your grade is C")
+elif score <=70:
+    print("Your grade is A")
